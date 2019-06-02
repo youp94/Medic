a846e64c1fe1d639a31eef633febecbc1b465f70
youcef <dm_benchadi@esi.dz>
2019-06-02 03:29:53 +0100

    design changes

--- app/src/main/java/com/medic/Activities/MainActivity.java
+++ app/src/main/java/com/medic/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.medic.Activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,69 +14,32 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.medic.Models.Constituant;
-import com.medic.Models.Formula;
-import com.medic.Models.Medic;
-import com.medic.Models.MedicForm;
+import com.google.firebase.database.ValueEventListener;
 import com.medic.Models.MedicalClass;
-import com.medic.Models.Process;
-import com.medic.Models.Step;
 import com.medic.R;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private DatabaseReference mDatabase;
-    MainAdapter mainAdapter = null;
-    ListView listView = null;
-
-    /***********************  Medical class 1 ***************************************************************/
 
     MedicalClass medicalClass1 = null;
+
     ArrayList<MedicalClass> medicalClassArrayList = new ArrayList<>();
-    ArrayList<Medic> medics = new ArrayList<>();
-
-    /************************************** Medicament 1 ****************************************************/
-
-    Medic medic1 = new Medic("", "Very long medic description text");
-    ArrayList<MedicForm> medic1Forms = new ArrayList<>();
-
-    /**************************************** Medicament 1 form 1 ************************************/
-
-    MedicForm medic1Form1 = new MedicForm("", "Very long medic form description text");
-    ArrayList<Process> processes11 = new ArrayList<>();
-
-    /********************************************* Medicament 1 form 1 process 1 ********************************/
-
-    Process process111 = new Process("", "Very long process description text");
-
-
-    /********************************************** Medicament 1 form 1 process formula *********************************/
-    Constituant constituant1111 = new Constituant(
-            "",
-            "",
-            "",
-            0,
-            "",
-            ""
-    );
-    ArrayList<Constituant> constituants111 = new ArrayList<>();
-
-    Step step1111 = new Step("");
-    ArrayList<Step> steps111 = new ArrayList<>();
-
-    Formula formula111 = new Formula("", "", "", constituants111, steps111,
-            "");
 
     /**********************************************************************************************************************/
 
 
-    /**********************************************************************************************************************/
+    MainAdapter mainAdapter = null;
+
+    ListView listView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,43 +48,29 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-
-        /********************************** form 1 ***************************************/
-
-
-        steps111.add(step1111);
-
-        constituants111.add(constituant1111);
-
-        process111.setFormula(formula111);
-
-        processes11.add(process111);
-        medic1Form1.setProcessArrayList(processes11);
-
-
-        medic1Forms.add(medic1Form1);
-        medic1.setMedicFormArrayList(medic1Forms);
-
-
-        medics.add(medic1);
-
-
-        Random rand = new Random();
-        int n = rand.nextInt(50000000);
-        medicalClass1 = new MedicalClass(String.valueOf(n), "", "Very long description text");
-
-        medicalClass1.setMedicArrayList(medics);
-
-
-        medicalClassArrayList.add(medicalClass1);
-
-
         mainAdapter = new MainAdapter(this, medicalClassArrayList);
 
         listView = findViewById(R.id.main_list);
         listView.setAdapter(mainAdapter);
 
-        //mDatabase.child("Medical Classes").child(String.valueOf(medicalClass1.getId())).setValue(medicalClass1);
+        mDatabase.child("Medical Classes").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> i = dataSnapshot.getChildren().iterator();
+                while (i.hasNext()) {
+                    MedicalClass medicalClass = i.next().getValue(MedicalClass.class);
+                    Toast.makeText(MainActivity.this, medicalClass.getName(), Toast.LENGTH_LONG).show();
+                    medicalClassArrayList.add(medicalClass);
+                    medicalClassArrayList.get(0).getId();
+                }
+                mainAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
 
@@ -162,7 +112,7 @@ class MainAdapter extends BaseAdapter{
 
         titleText = view.findViewById(R.id.class_title);
         button = view.findViewById(R.id.class_button);
-        button.setText("view drugs");
+        button.setText("View Drugs");
 
         titleText.setText(medicalClass.getName());
         button.setOnClickListener(new View.OnClickListener() {
